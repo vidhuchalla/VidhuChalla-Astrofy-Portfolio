b78ecce2e14e516d971f28cd7a6ee0dd9859434a
vidhuchalla <vidhu.challa@gmail.com>
2026-07-17 22:18:22 -0700

    Disable sitemap to fix build error and finalize config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://vidhuchalla.github.io',
   base: '/VidhuChalla-Astrofy-Portfolio/',
-  integrations: [mdx(), sitemap(), tailwind()] 
+  integrations: [mdx(), tailwind()] // Temporarily removed sitemap()
 });
