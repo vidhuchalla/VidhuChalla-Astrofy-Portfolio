dab5cd825fb636c2e0ac7b0cc22cb1d1f18c0480
vidhuchalla <vidhu.challa@gmail.com>
2026-07-17 21:18:12 -0700

    first commit

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,11 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
-export const GENERATE_SLUG_FROM_TITLE = true
-export const TRANSITION_API = true
+export const SITE_TITLE = 'Vidhu Challa | Portfolio';
+
+export const SITE_DESCRIPTION =
+  'Personal portfolio of Vidhu Challa, a high school student interested in computer science, artificial intelligence, and software development.';
+
+export const GENERATE_SLUG_FROM_TITLE = true;
+
+export const TRANSITION_API = true;
