1ea69f734872d40600ff22e0b0b5cf5614d3bdd7
vidhuchalla <vidhu.challa@gmail.com>
2026-07-17 21:32:32 -0700

    Disable sitemap to fix build error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import sitemap from '@astrojs/sitemap'; // Keep this import
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astrofy-template.netlify.app',
-  integrations: [mdx(), sitemap(), tailwind()]
+  // Comment out or remove sitemap() from the array below:
+  integrations: [mdx(), /*sitemap(),*/ tailwind()] 
 });
